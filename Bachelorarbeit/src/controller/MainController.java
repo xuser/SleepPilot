48dc54d03327208057db10085b0632b950200f9d
nfinke <finken@informatik.uni-luebeck.de>
2014-04-05 21:32:19 +0200

    Erstellung des Maincontrollers ohne jegliche Funktion.

---
+++ Bachelorarbeit/src/controller/MainController.java
@@ -0,0 +1,23 @@
+/**
+ * Contains all classes which controll programm flow.
+ */
+package controller;
+
+/**
+ * Starts the application and creates necessary initial controllers.
+ * 
+ * @author Nils Finke
+ */
+public class MainController {
+
+	/**
+	 * Starts the application with the needed parameters.
+	 * 
+	 * @param args
+	 * 			no starting arguments are needed.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
