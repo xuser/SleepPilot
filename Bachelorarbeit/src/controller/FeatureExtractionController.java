31aab0fe252f2800c9363a3f1797240c673875e1
nfinke <finken@informatik.uni-luebeck.de>
2014-05-01 18:35:13 +0200

    Eine neue Klasse von der aus die einzelnen Methoden der Feature Extraction aufgerufen werden sollen, wurde hinzugefügt. Diese enthält allerdings noch keinerlei Implementierung.

---
+++ Bachelorarbeit/src/controller/FeatureExtractionController.java
@@ -0,0 +1,21 @@
+package controller;
+
+/**
+ * This class calls the feature extraction methods for extracting relevant features to create 
+ * feature vectors, which can be used for the support vector maschine.
+ * 
+ * @author Nils Finke
+ *
+ */
+public class FeatureExtractionController {
+	
+	
+	/**
+	 * Constructor which initializes the class.
+	 */
+	public FeatureExtractionController(){
+		
+	}
+	
+
+}
