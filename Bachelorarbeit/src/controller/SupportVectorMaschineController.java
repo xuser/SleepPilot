14689f93fee6cf04584c9f6dd78f2d4bb594dee8
nfinke <finken@informatik.uni-luebeck.de>
2014-05-09 13:12:20 +0200

    Mit der Implementierung der Support Vector Maschine wurde begonnnen. Im help Package wurden zusätzlich Klassen, die im Rahmen des LIBSVM Frameworks bereitgestellt werden, hinzugefügt. Diese dienen beispielsweise zum Skalieren der Daten.

--- Bachelorarbeit/src/controller/SupportVectorMaschineController.java
+++ Bachelorarbeit/src/controller/SupportVectorMaschineController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import sun.security.jca.GetInstance.Instance;
+import libsvm.*;
+
 /**
  * This class starts the support vector maschine and uses the LIBSVM framework.
  * 
@@ -11,6 +14,13 @@ public class SupportVectorMaschineController {
 	 * This constructor initializes the class.
 	 */
 	public SupportVectorMaschineController() {
+		
+		// Setup parameters
+		svm_parameter parameter = new svm_parameter();
+		
+		// Register the kernel function
+		parameter.kernel_type = svm_parameter.RBF;
+		
 		
 	}
 }
