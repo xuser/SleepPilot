f3fffcb87efaadfab740ea28c9fd43d18137d958
nfinke <finken@informatik.uni-luebeck.de>
2014-06-21 18:57:18 +0200

    Zweites Fenster für zusätzliche Visualisierungen hinzugefügt.

--- Bachelorarbeit/src/view/ApplicationController.java
+++ Bachelorarbeit/src/view/ApplicationController.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -17,6 +19,8 @@ public class ApplicationController implements Initializable{
 	private BorderPane mainGrid;
 	
 	private Scene scene;
+	
+	@FXML MenuItem showAdtVisualization;
 	
 	public ApplicationController(Stage stage) {
 		primaryStage = stage;
@@ -30,7 +34,7 @@ public class ApplicationController implements Initializable{
 			mainGrid = loader.load();
 		} catch (IOException e) {
 			System.err.println("Error during loading Application.fxml file!");
-//			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		
 		// Create stage with mainGrid
@@ -48,5 +52,30 @@ public class ApplicationController implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 	}
-
+	
+	@FXML
+	protected void showAdtVisualizationAction() {
+		
+		Stage stage = new Stage();
+		BorderPane addGrid = new BorderPane();
+		
+		// Creating FXML Loader
+		FXMLLoader loader = new FXMLLoader(StartController.class.getResource("AdditionalVisualization.fxml"));
+		loader.setController(this);
+		
+		// Try to load fxml file
+		try {
+			addGrid = loader.load();
+		} catch (IOException e) {
+			System.err.println("Error during loading AdditionalVisualization.fxml file!");
+			//e.printStackTrace();
+		}
+		
+		Scene scene = new Scene(addGrid);
+		
+		stage.setScene(scene);
+		stage.show();
+		stage.setTitle("Additional Visualization");
+		
+	}
 }
