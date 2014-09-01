3b2af3e2ba6c4c19096b71bd99d8d5b6e9594ffe
nfinke <finken@informatik.uni-luebeck.de>
2014-09-01 15:50:59 +0200

    Das Model für mit Header-Informationen wurde um die Informationen, die im Spike2 Format benötigt werden, ergänzt. Entsprechend wurden auch Getter und Setter Methoden angelegt.

--- Bachelorarbeit/src/model/DataPoints.java
+++ Bachelorarbeit/src/model/DataPoints.java
@@ -43,15 +43,65 @@ public class DataPoints {
 	 */
 	private int rowFilteredValues = 0;
 	
+	// *** Brainvision Format ***
 	// [Common Infos]
 	private int numberOfDataPoints;
-	private int numberOfChannels;
+	private int numberOfChannels;					// This variable will be used for the Spike2 Format as well
 	private int samplingIntervall;
 	
 	// [Channel Infos]
 	private String[] channelNames;
 	
+	// *** Brainvision Format ***
+	
 	private int samplingRateConvertedToHertz;
+	
+	// *** Spike2 Format ***
+	
+	// Information from File Header
+	private static int systemId;
+	private static int usPerTime;
+	private static int timePerADC;
+	private static int fileState;
+	private static int firstData;
+	private static int channels;
+	private static int chanSize;
+	private static int extraData;
+	private static int bufferSize;
+	private static int osFormat;
+	private static int maxFTime;
+	private static double dTimeBase;
+
+	// Information from Channel Header
+
+	// We dont know the exact number of channels. This is why we create lists,
+	// which will be filled up. Example: All information for channel one are in
+	// list position zero etc..
+	private static LinkedList<Integer> delSize = new LinkedList<Integer>();
+	private static LinkedList<Integer> nextDelBlock = new LinkedList<Integer>();
+	private static LinkedList<Integer> firstBlock = new LinkedList<Integer>();
+	private static LinkedList<Integer> lastBlock = new LinkedList<Integer>();
+	private static LinkedList<Integer> blocks = new LinkedList<Integer>();
+	private static LinkedList<Integer> nExtra = new LinkedList<Integer>();
+	private static LinkedList<Integer> preTrig = new LinkedList<Integer>();
+	private static LinkedList<Integer> free0 = new LinkedList<Integer>();
+	private static LinkedList<Integer> phySz = new LinkedList<Integer>();
+	private static LinkedList<Integer> maxData = new LinkedList<Integer>();
+	private static LinkedList<Integer> maxChanTime = new LinkedList<Integer>();
+	private static LinkedList<Integer> lChanDvd = new LinkedList<Integer>();
+	private static LinkedList<Integer> phyChan = new LinkedList<Integer>();
+	private static LinkedList<String> titel = new LinkedList<String>();
+	private static LinkedList<Float> idealRate = new LinkedList<Float>();
+	private static LinkedList<Integer> kind = new LinkedList<Integer>();
+	private static LinkedList<Integer> pad = new LinkedList<Integer>();
+	private static LinkedList<Float> scale = new LinkedList<Float>();
+	private static LinkedList<Float> offset = new LinkedList<Float>();
+	private static LinkedList<Integer> divide = new LinkedList<Integer>();
+	private static LinkedList<Integer> interleave = new LinkedList<Integer>();
+	
+	
+	// *** Spike2 Format ***
+	
 	
 	/**
 	 * @param dataPoints the dataPoints to set
@@ -150,6 +200,8 @@ public class DataPoints {
 	}
 	
 	/**
+	 * NOT USED ANY LONGER!
+	 * 
 	 * This method returns a value from a specific position in the data points.
 	 * IMPORTANT: Just use this function for filtering and not, if you want to print the value!
 	 * 
@@ -166,6 +218,8 @@ public class DataPoints {
 	
 	
 	/**
+	 * NOT USED ANY LONGER!
+	 * 
 	 * Use this method to get all samples from one 30s epoch. The iteration over
 	 * the different epochs have to be done in the controller class.
 	 * 
@@ -274,6 +328,468 @@ public class DataPoints {
 	
 	public int getSizeOfFilteredEpochList() {
 		return filteredEpochs.size();
+	}
+
+	/**
+	 * @return the systemId
+	 */
+	public static int getSystemId() {
+		return systemId;
+	}
+
+	/**
+	 * @param systemId the systemId to set
+	 */
+	public static void setSystemId(int systemId) {
+		DataPoints.systemId = systemId;
+	}
+
+	/**
+	 * @return the usPerTime
+	 */
+	public static int getUsPerTime() {
+		return usPerTime;
+	}
+
+	/**
+	 * @param usPerTime the usPerTime to set
+	 */
+	public static void setUsPerTime(int usPerTime) {
+		DataPoints.usPerTime = usPerTime;
+	}
+
+	/**
+	 * @return the timePerADC
+	 */
+	public static int getTimePerADC() {
+		return timePerADC;
+	}
+
+	/**
+	 * @param timePerADC the timePerADC to set
+	 */
+	public static void setTimePerADC(int timePerADC) {
+		DataPoints.timePerADC = timePerADC;
+	}
+
+	/**
+	 * @return the fileState
+	 */
+	public static int getFileState() {
+		return fileState;
+	}
+
+	/**
+	 * @param fileState the fileState to set
+	 */
+	public static void setFileState(int fileState) {
+		DataPoints.fileState = fileState;
+	}
+
+	/**
+	 * @return the firstData
+	 */
+	public static int getFirstData() {
+		return firstData;
+	}
+
+	/**
+	 * @param firstData the firstData to set
+	 */
+	public static void setFirstData(int firstData) {
+		DataPoints.firstData = firstData;
+	}
+
+	/**
+	 * @return the channels
+	 */
+	public static int getChannels() {
+		return channels;
+	}
+
+	/**
+	 * @param channels the channels to set
+	 */
+	public static void setChannels(int channels) {
+		DataPoints.channels = channels;
+	}
+
+	/**
+	 * @return the chanSize
+	 */
+	public static int getChanSize() {
+		return chanSize;
+	}
+
+	/**
+	 * @param chanSize the chanSize to set
+	 */
+	public static void setChanSize(int chanSize) {
+		DataPoints.chanSize = chanSize;
+	}
+
+	/**
+	 * @return the extraData
+	 */
+	public static int getExtraData() {
+		return extraData;
+	}
+
+	/**
+	 * @param extraData the extraData to set
+	 */
+	public static void setExtraData(int extraData) {
+		DataPoints.extraData = extraData;
+	}
+
+	/**
+	 * @return the bufferSize
+	 */
+	public static int getBufferSize() {
+		return bufferSize;
+	}
+
+	/**
+	 * @param bufferSize the bufferSize to set
+	 */
+	public static void setBufferSize(int bufferSize) {
+		DataPoints.bufferSize = bufferSize;
+	}
+
+	/**
+	 * @return the osFormat
+	 */
+	public static int getOsFormat() {
+		return osFormat;
+	}
+
+	/**
+	 * @param osFormat the osFormat to set
+	 */
+	public static void setOsFormat(int osFormat) {
+		DataPoints.osFormat = osFormat;
+	}
+
+	/**
+	 * @return the maxFTime
+	 */
+	public static int getMaxFTime() {
+		return maxFTime;
+	}
+
+	/**
+	 * @param maxFTime the maxFTime to set
+	 */
+	public static void setMaxFTime(int maxFTime) {
+		DataPoints.maxFTime = maxFTime;
+	}
+
+	/**
+	 * @return the dTimeBase
+	 */
+	public static double getdTimeBase() {
+		return dTimeBase;
+	}
+
+	/**
+	 * @param dTimeBase the dTimeBase to set
+	 */
+	public static void setdTimeBase(double dTimeBase) {
+		DataPoints.dTimeBase = dTimeBase;
+	}
+
+	/**
+	 * @return the delSize
+	 */
+	public static LinkedList<Integer> getDelSize() {
+		return delSize;
+	}
+
+	/**
+	 * @param delSize the delSize to set
+	 */
+	public static void setDelSize(LinkedList<Integer> delSize) {
+		DataPoints.delSize = delSize;
+	}
+
+	/**
+	 * @return the nextDelBlock
+	 */
+	public static LinkedList<Integer> getNextDelBlock() {
+		return nextDelBlock;
+	}
+
+	/**
+	 * @param nextDelBlock the nextDelBlock to set
+	 */
+	public static void setNextDelBlock(LinkedList<Integer> nextDelBlock) {
+		DataPoints.nextDelBlock = nextDelBlock;
+	}
+
+	/**
+	 * @return the firstBlock
+	 */
+	public static LinkedList<Integer> getFirstBlock() {
+		return firstBlock;
+	}
+
+	/**
+	 * @param firstBlock the firstBlock to set
+	 */
+	public static void setFirstBlock(LinkedList<Integer> firstBlock) {
+		DataPoints.firstBlock = firstBlock;
+	}
+
+	/**
+	 * @return the lastBlock
+	 */
+	public static LinkedList<Integer> getLastBlock() {
+		return lastBlock;
+	}
+
+	/**
+	 * @param lastBlock the lastBlock to set
+	 */
+	public static void setLastBlock(LinkedList<Integer> lastBlock) {
+		DataPoints.lastBlock = lastBlock;
+	}
+
+	/**
+	 * @return the blocks
+	 */
+	public static LinkedList<Integer> getBlocks() {
+		return blocks;
+	}
+
+	/**
+	 * @param blocks the blocks to set
+	 */
+	public static void setBlocks(LinkedList<Integer> blocks) {
+		DataPoints.blocks = blocks;
+	}
+
+	/**
+	 * @return the nExtra
+	 */
+	public static LinkedList<Integer> getnExtra() {
+		return nExtra;
+	}
+
+	/**
+	 * @param nExtra the nExtra to set
+	 */
+	public static void setnExtra(LinkedList<Integer> nExtra) {
+		DataPoints.nExtra = nExtra;
+	}
+
+	/**
+	 * @return the preTrig
+	 */
+	public static LinkedList<Integer> getPreTrig() {
+		return preTrig;
+	}
+
+	/**
+	 * @param preTrig the preTrig to set
+	 */
+	public static void setPreTrig(LinkedList<Integer> preTrig) {
+		DataPoints.preTrig = preTrig;
+	}
+
+	/**
+	 * @return the free0
+	 */
+	public static LinkedList<Integer> getFree0() {
+		return free0;
+	}
+
+	/**
+	 * @param free0 the free0 to set
+	 */
+	public static void setFree0(LinkedList<Integer> free0) {
+		DataPoints.free0 = free0;
+	}
+
+	/**
+	 * @return the phySz
+	 */
+	public static LinkedList<Integer> getPhySz() {
+		return phySz;
+	}
+
+	/**
+	 * @param phySz the phySz to set
+	 */
+	public static void setPhySz(LinkedList<Integer> phySz) {
+		DataPoints.phySz = phySz;
+	}
+
+	/**
+	 * @return the maxData
+	 */
+	public static LinkedList<Integer> getMaxData() {
+		return maxData;
+	}
+
+	/**
+	 * @param maxData the maxData to set
+	 */
+	public static void setMaxData(LinkedList<Integer> maxData) {
+		DataPoints.maxData = maxData;
+	}
+
+	/**
+	 * @return the maxChanTime
+	 */
+	public static LinkedList<Integer> getMaxChanTime() {
+		return maxChanTime;
+	}
+
+	/**
+	 * @param maxChanTime the maxChanTime to set
+	 */
+	public static void setMaxChanTime(LinkedList<Integer> maxChanTime) {
+		DataPoints.maxChanTime = maxChanTime;
+	}
+
+	/**
+	 * @return the lChanDvd
+	 */
+	public static LinkedList<Integer> getlChanDvd() {
+		return lChanDvd;
+	}
+
+	/**
+	 * @param lChanDvd the lChanDvd to set
+	 */
+	public static void setlChanDvd(LinkedList<Integer> lChanDvd) {
+		DataPoints.lChanDvd = lChanDvd;
+	}
+
+	/**
+	 * @return the phyChan
+	 */
+	public static LinkedList<Integer> getPhyChan() {
+		return phyChan;
+	}
+
+	/**
+	 * @param phyChan the phyChan to set
+	 */
+	public static void setPhyChan(LinkedList<Integer> phyChan) {
+		DataPoints.phyChan = phyChan;
+	}
+
+	/**
+	 * @return the titel
+	 */
+	public static LinkedList<String> getTitel() {
+		return titel;
+	}
+
+	/**
+	 * @param titel the titel to set
+	 */
+	public static void setTitel(LinkedList<String> titel) {
+		DataPoints.titel = titel;
+	}
+
+	/**
+	 * @return the idealRate
+	 */
+	public static LinkedList<Float> getIdealRate() {
+		return idealRate;
+	}
+
+	/**
+	 * @param idealRate the idealRate to set
+	 */
+	public static void setIdealRate(LinkedList<Float> idealRate) {
+		DataPoints.idealRate = idealRate;
+	}
+
+	/**
+	 * @return the kind
+	 */
+	public static LinkedList<Integer> getKind() {
+		return kind;
+	}
+
+	/**
+	 * @param kind the kind to set
+	 */
+	public static void setKind(LinkedList<Integer> kind) {
+		DataPoints.kind = kind;
+	}
+
+	/**
+	 * @return the pad
+	 */
+	public static LinkedList<Integer> getPad() {
+		return pad;
+	}
+
+	/**
+	 * @param pad the pad to set
+	 */
+	public static void setPad(LinkedList<Integer> pad) {
+		DataPoints.pad = pad;
+	}
+
+	/**
+	 * @return the scale
+	 */
+	public static LinkedList<Float> getScale() {
+		return scale;
+	}
+
+	/**
+	 * @param scale the scale to set
+	 */
+	public static void setScale(LinkedList<Float> scale) {
+		DataPoints.scale = scale;
+	}
+
+	/**
+	 * @return the offset
+	 */
+	public static LinkedList<Float> getOffset() {
+		return offset;
+	}
+
+	/**
+	 * @param offset the offset to set
+	 */
+	public static void setOffset(LinkedList<Float> offset) {
+		DataPoints.offset = offset;
+	}
+
+	/**
+	 * @return the divide
+	 */
+	public static LinkedList<Integer> getDivide() {
+		return divide;
+	}
+
+	/**
+	 * @param divide the divide to set
+	 */
+	public static void setDivide(LinkedList<Integer> divide) {
+		DataPoints.divide = divide;
+	}
+
+	/**
+	 * @return the interleave
+	 */
+	public static LinkedList<Integer> getInterleave() {
+		return interleave;
+	}
+
+	/**
+	 * @param interleave the interleave to set
+	 */
+	public static void setInterleave(LinkedList<Integer> interleave) {
+		DataPoints.interleave = interleave;
 	}
 
 
