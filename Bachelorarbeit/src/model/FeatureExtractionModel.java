9a631f81c2fcaa15c8e164c115f46c56ab8c142f
nfinke <finken@informatik.uni-luebeck.de>
2014-10-12 22:07:06 +0200

    Changed button dependencies

--- Bachelorarbeit/src/model/FeatureExtractionModel.java
+++ Bachelorarbeit/src/model/FeatureExtractionModel.java
@@ -135,11 +135,13 @@ public class FeatureExtractionModel implements Serializable{
 	public void addArtefactToEpochProperty(int epoch) {
 		if (epochProperties.containsKey(epoch)) {
 			int tmpMAFlag = 0;
+			boolean aButtonWasActiveFlag = false;
 			
 			Integer[] tempProp = epochProperties.get(epoch);
 			epochProperties.remove(epoch);
 			
 			if (tempProp[0] == 1) {
+				aButtonWasActiveFlag = true;
 				countA--;
 			}
 			
@@ -152,14 +154,25 @@ public class FeatureExtractionModel implements Serializable{
 				countS--;
 			}
 			
-			Integer[] prop = {1,tmpMAFlag,0};
-			countA++;
-			
-			if (tmpMAFlag == 1) {
-				countMA++;
+			if (aButtonWasActiveFlag) {
+				Integer[] prop = {0, tmpMAFlag, 0};
+				if (tmpMAFlag == 1) {
+					countMA++;
+				}
+				
+				epochProperties.put(epoch, prop);
+				
+			} else {
+				Integer[] prop = {1,tmpMAFlag,0};
+				countA++;
+				
+				if (tmpMAFlag == 1) {
+					countMA++;
+				}
+				
+				epochProperties.put(epoch, prop);
+				
 			}
-			
-			epochProperties.put(epoch, prop);
 			
 		} else {
 			Integer[] prop = {1,0,0};
@@ -170,35 +183,50 @@ public class FeatureExtractionModel implements Serializable{
 	
 	public void addArrousalToEpochProperty(int epoch) {
 		if (epochProperties.containsKey(epoch)) {
-			int tmpAFlag = 0;
+			
+			boolean aButtonWasActiveFlag = false;
+			boolean maButtonWasActiveFlag = false;
 			
 			Integer[] tempProp = epochProperties.get(epoch);
-			epochProperties.remove(epoch);
+			epochProperties.remove(epoch);			
 			
 			if (tempProp[0] == 1) {
+				aButtonWasActiveFlag = true;
 				countA--;
-				tmpAFlag = 1;
 			}
 			
 			if (tempProp[1] == 1) {
+				maButtonWasActiveFlag = true;
 				countMA--;
 			}
 			
 			if (tempProp[2] == 1) {
 				countS--;
-			}
+			}			
 			
-			Integer[] prop = {tmpAFlag,1,0};
-			countMA++;
-			
-			if (tmpAFlag == 1) {
+			if (maButtonWasActiveFlag) {
+				
+				if (aButtonWasActiveFlag) {
+					Integer[] prop = {1,0,0};
+					countA++;
+					epochProperties.put(epoch, prop);
+
+				} else {					
+					Integer[] prop = {0,0,0};
+					epochProperties.put(epoch, prop);
+				}
+				
+			} else {
+				Integer[] prop = {1,1,0};			
 				countA++;
+				countMA++;		
+				
+				epochProperties.put(epoch, prop);
 			}
-			
-			epochProperties.put(epoch, prop);
 			
 		} else {
-			Integer[] prop = {0,1,0};
+			Integer[] prop = {1,1,0};
+			countA++;
 			countMA++;
 			epochProperties.put(epoch, prop);
 		}
@@ -206,6 +234,9 @@ public class FeatureExtractionModel implements Serializable{
 	
 	public void addStimulationToEpochProperty(int epoch) {
 		if (epochProperties.containsKey(epoch)) {
+			
+			boolean sButtonWasActiveFlag = false;
+			
 			Integer[] tempProp = epochProperties.get(epoch);
 			epochProperties.remove(epoch);
 			
@@ -218,29 +249,50 @@ public class FeatureExtractionModel implements Serializable{
 			}
 			
 			if (tempProp[2] == 1) {
+				sButtonWasActiveFlag = true;
 				countS--;
 			}
 			
-			Integer[] prop = {0,0,1};
-			countS++;
-			
-			int label = getFeatureClassLabel(epoch);
-			switch (label) {
-			case 1: countWake--;
-				break;
-			case 2: countS1--;
-				break;
-			case 3: countS2--;
-				break;
-			case 4: countN--;
-				break;
-			case 5: countREM--;
-				break;
-			default: System.out.println("No class label set!");
-				break;
+			if (sButtonWasActiveFlag) {
+				
+				int label = getFeatureClassLabel(epoch);
+				switch (label) {
+				case 1: countWake++;
+					break;
+				case 2: countS1++;
+					break;
+				case 3: countS2++;
+					break;
+				case 4: countN++;
+					break;
+				case 5: countREM++;
+					break;
+				default: System.out.println("No class label set!");
+					break;
+				}
+				
+			} else {
+				Integer[] prop = {0,0,1};
+				countS++;
+				
+				int label = getFeatureClassLabel(epoch);
+				switch (label) {
+				case 1: countWake--;
+					break;
+				case 2: countS1--;
+					break;
+				case 3: countS2--;
+					break;
+				case 4: countN--;
+					break;
+				case 5: countREM--;
+					break;
+				default: System.out.println("No class label set!");
+					break;
+				}
+				
+				epochProperties.put(epoch, prop);
 			}
-			
-			epochProperties.put(epoch, prop);
 			
 		} else {
 			Integer[] prop = {0,0,1};
